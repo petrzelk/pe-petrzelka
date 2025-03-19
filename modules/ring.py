3a5215edcc7420da972490b28a89b43667f6b859
John Petrzelka <johnpetrzelka@gmail.com>
2025-03-18 20:30:23 -0500

    Fix for formating of the modules/ring.py file

--- modules/ring.py
+++ modules/ring.py
@@ -1,23 +1,26 @@
 class ring():
-    def __init__(self, values:list ):
-        self.values = values
-        self.len=len(values)
-    def __getitem__(self, key):
-        if isinstance(key, int):
-            return(self.values[key%self.len])
-        elif isinstance(key, slice):
-            if key.start!=None and key.stop!=None and key.step!=None:
-                return([self.values[i%self.len] for i in range(key.start,key.stop,key.step)])
-            elif key.start!=None and key.stop!=None:
-                return([self.values[i%self.len] for i in range(key.start,key.stop)])
-            else:
-                # Behavior for no start/end not implimented.
-                raise( "IndexError: no start and ending given")
-        else:
-            raise( "IndexError: indices not supported" )
+  """A custom psuado-mathematical ring class """
 
-    def __setitem__(self, key, newvalue):
-        self.values[key%self.len]=newvalue
+  def __init__(self, values:list ):
+    self.values = values
+    self.len=len(values)
 
-    def __contains__(self, item):
-        return item in self.values
+  def __getitem__(self, key):
+    if isinstance(key, int):
+      return(self.values[key%self.len])
+    elif isinstance(key, slice):
+      if key.start!=None and key.stop!=None and key.step!=None:
+        return([self.values[i%self.len] for i in range(key.start,key.stop,key.step)])
+      elif key.start!=None and key.stop!=None:
+        return([self.values[i%self.len] for i in range(key.start,key.stop)])
+      else:
+        # Behavior for no start/end not implimented.
+        raise( "IndexError: no start and ending given")
+    else:
+      raise( "IndexError: indices not supported" )
+
+  def __setitem__(self, key, newvalue):
+    self.values[key%self.len]=newvalue
+
+  def __contains__(self, item):
+    return item in self.values
